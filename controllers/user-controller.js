bbd3794788d5fa5915714625bd99832b5066d7dc
Fatima Gomez <fatima3558@gmail.com>
2019-07-29 09:57:58 -0500

    registration creates new user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,28 +1,57 @@
 const express = require("express")
+const bcrypt = require("bcryptjs")
 const router = express.Router()
 
 const User = require("../models/user")
 
 //index
 router.get("/", (req, res, next) => {
-	res.redirect("/user/login")
+	res.render("users/login.ejs",)
 })
 
 /**AUTHENTICATION**/
 //login
-router.get("/login", (req, res, next) => {
-	res.render("users/login.ejs")
+router.post("/login", (req, res, next) => {
+	res.send("check once registration is complete")
 })
 
 //register
 router.get("/register", (req, res, next) => {
-	res.render("users/register.ejs")
+	res.render("users/register.ejs", {
+		message: req.session.message
+	})
+})
+
+//new
+router.post("/new", async (req, res, next) => {
+	// const duplicateUser = await User.find({name: req.body.name})
+	try {
+		// if(!duplicateUser) {
+			//secure password
+			const password = req.body.password
+			const hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+			req.body.password = hashedPassword
+			//create user
+			const newUser = await User.create(req.body)
+			//set session
+			req.session.userId = newUser._id
+			req.session.username = newUser.name
+			req.session.loggedIn = true
+			// redirect
+			res.send("created account successfully")
+		// } else {
+			// req.session.message = "That username already exists. Try another one."
+		// }
+	} catch(err) {
+		next(err)
+	}
 })
 
 //logout
 
+
 /**REST**/
-//show 
+//show
 
 //edit -- to edit own profile
 
