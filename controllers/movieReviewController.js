5e173f341f0ee0b88d4b3fbe66ab472938cd66ed
Pavitra Brahmbhatt <pavitrabrahmbhatt@Pavitras-MacBook-Air.local>
2019-07-30 15:52:25 -0500

    User can search for movie and see movie show page with movie info

--- controllers/movieReviewController.js
+++ controllers/movieReviewController.js
@@ -8,40 +8,35 @@ router.use(requireAuth)
 
 //show
  router.get('/', (req, res, next) => {
- 	const value = "Mean Girls"
-    const url = 'http://www.omdbapi.com/?t='+ value + '&apikey='+ process.env.API_KEY
-    console.log(url);
-    //use superagent to request api data
-    superagent
-    .get(url)
-    .end((error, response) => {
-      if(error) next(error);
-      else {
-         console.dir(response) // <-- format of this response will vary WIDELY depending on the API you're working with
-         console.dir(response.text); // <-- this appears to be the JSON we want
-      
-        const dataAsObj = JSON.parse(response.text)
-        console.dir(dataAsObj)
-
-  
-        const title = dataAsObj.Title
-        const year = dataAsObj.Year
-        const genre = dataAsObj.Genre
-		    const poster = dataAsObj.Poster
-		    const plot = dataAsObj.Plot
-
-      
+  // console.log(req.query.title)
+  const url = 'http://www.omdbapi.com/?t='+ req.query.title + '&apikey='+ process.env.API_KEY
+  console.log(url);
+  //use superagent to request api data
+  superagent
+  .get(url)
+  .end((error, response) => {
+    if(error) next(error);
+    else {
+      console.dir(response) // <-- format of this response will vary WIDELY depending on the API you're working with
+      console.dir(response.text); // <-- this appears to be the JSON we want
     
-	     console.log(" movie show route");
-	     //res.redirect("movies/")
+      const dataAsObj = JSON.parse(response.text)
+     //  console.dir(dataAsObj)
 
-    	res.render('movies/show.ejs', {
-        title: title,
-        year: year,
-        genre: genre,
-        poster: poster,
-        plot: plot
-
+     //  const title = dataAsObj.Title
+     //  const year = dataAsObj.Year
+     //  const genre = dataAsObj.Genre
+	    // const poster = dataAsObj.Poster
+	    // const plot = dataAsObj.Plot
+     //  console.log(" movie show route");
+      //res.redirect("movies/")
+      
+      res.render('movies/show.ejs', {
+        title: dataAsObj.Title,
+        year: dataAsObj.Year,
+        genre: dataAsObj.Genre,
+        poster: dataAsObj.Poster,
+        plot: dataAsObj.Plot
       })
     }
   })
