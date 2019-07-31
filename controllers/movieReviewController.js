df68c78c0f9cb311ecc3af7d992e9c0102812792
Pavitra Brahmbhatt <pavitrabrahmbhatt@Pavitras-MacBook-Air.local>
2019-07-30 19:51:58 -0500

    review route saves movie in data base

--- controllers/movieReviewController.js
+++ controllers/movieReviewController.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const requireAuth = require("../lib/requireAuth.js")
 const superagent = require('superagent')
 const Movie = require("../models/movie")
+require('isomorphic-fetch')
+require('es6-promise')
 
 router.use(requireAuth)
 
@@ -10,15 +12,15 @@ router.use(requireAuth)
  router.get('/', (req, res, next) => {
   // console.log(req.query.title)
   const url = 'http://www.omdbapi.com/?t='+ req.query.title + '&apikey='+ process.env.API_KEY
-  console.log(url);
+  //console.log(url);
   //use superagent to request api data
   superagent
   .get(url)
   .end((error, response) => {
     if(error) next(error);
     else {
-      console.dir(response) // <-- format of this response will vary WIDELY depending on the API you're working with
-      console.dir(response.text); // <-- this appears to be the JSON we want
+      // console.dir(response) // <-- format of this response will vary WIDELY depending on the API you're working with
+      // console.dir(response.text); // <-- this appears to be the JSON we want
     
       const dataAsObj = JSON.parse(response.text)
     
@@ -35,27 +37,76 @@ router.use(requireAuth)
   })
 })
 
+//home
 router.get("/feed", (req, res, next) => {
 	res.render("reviews/feed.ejs")
 })
 
+//show all movies
 router.get("/movieIndex", (req,res,next) => {
 	res.render("movies/index.ejs")
 })
 
-router.get("/:id/create", (req,res,next) => {
-	res.render("reviews/new.ejs")
+//new review
+router.get("/:id/create", async (req,res,next) => {
+	res.render("reviews/new.ejs", {
+    imdbID: req.params.id
+  })
 })
 
+//edit review
 router.get("/edit", (req,res,next) => {
 	res.render("reviews/edit.ejs")
 })
 
-router.get("/:id", ((req,res,next) => {
+//show single movie page
+router.get("/:id", (req,res,next) => {
+  //find all reviews with this movie's imdbID
   res.render("movies/show.ejs")
-}))
+})
+
+//post review to movie
+router.post("/:id", async (req,res,next) => {
+  //add movie to database when first review is created
+  try {
+    const url = await fetch('http://www.omdbapi.com/?i='+ req.params.id + '&apikey='+ process.env.API_KEY)
+    // superagent
+    // .get(url)
+    // .end( async (error, response) => {
+    //   if(error) next(error);
+      // else {
+        //console.dir(response) // <-- format of this response will vary WIDELY depending on the API you're working with
+        //console.dir(response.text); // <-- this appears to be the JSON we want
+      
+        const dataAsObj = await url.json()
+        console.log(dataAsObj.Genre);
+        console.log( "about to create movie");
+        const newMovie ={
+          title: dataAsObj.Title, 
+          year: dataAsObj.Year,
+          genre: dataAsObj.Genre,
+          poster: dataAsObj.Poster,
+          plot: dataAsObj.Plot
+        }
+        const reviewedMovie = await Movie.create(
+          newMovie
+        )
+
+        reviewedMovie.save()
+      // }
+    // }) 
+    console.log("check mongodb") 
 
 
+    //assign database id to review
+
+    //assign user id to review
+    //redirect to show page
+
+  } catch(error) {
+    next(error)
+  }
+})
 
 
 module.exports = router
